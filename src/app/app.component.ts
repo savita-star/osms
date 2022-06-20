496774b0c4808627b4ed60b06c60e83e1a984831
Savita <82285804+savita-star@users.noreply.github.com>
2022-06-20 16:36:47 -0400

    added the student data store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { StudentService } from './services/student.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'osms';
+
+  constructor(private studentService: StudentService) {
+    this.studentService.getStudents().subscribe((students) => {
+      console.log('Get Students From Service', students);
+      console.log(
+        'Before Add New Student',
+        this.studentService.getStudentsData()
+      );
+      this.studentService.addStudent({
+        id: 10,
+        firstName: 'Test',
+        lastname: 'Test',
+        marks: 0,
+        attendence: false,
+      });
+      console.log(
+        'After Add New Student',
+        this.studentService.getStudentsData()
+      );
+
+      console.log(
+        'Before Remove Student',
+        this.studentService.getStudentsData()
+      );
+      this.studentService.removeStudent(10);
+      console.log(
+        'After Remove Student',
+        this.studentService.getStudentsData()
+      );
+
+      console.log(
+        'Get Single Student Record',
+        this.studentService.getStudent(5)
+      );
+    });
+  }
 }
---
+++ src/app/models/student.ts
@@ -0,0 +1,7 @@
+export interface IStudent {
+  id: number;
+  firstName: string;
+  lastname: string;
+  marks: number;
+  attendence: boolean;
+}

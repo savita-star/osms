1144f9395f6510e9e3f7182622bd22d800c50db5
Vishva Patel <1996vishva@gmail.com>
2022-06-21 21:15:08 -0400

    added nav-bar component to the home page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MarksComponent } from './marks/marks.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    MarksComponent,
+    NavBarComponent,
+      ],
   imports: [
     BrowserModule,
     AppRoutingModule
